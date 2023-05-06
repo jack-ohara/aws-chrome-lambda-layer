f6904f0ef8951d12bbc9674c5fad10de2a9e7eba
Jack O'Hara <jack.ohara@infinityworks.com>
2023-05-06 08:27:57 +0100

    feat: initial version of lambda layer

--- lib/aws-chrome-lambda-layer-stack.ts
+++ lib/aws-chrome-lambda-layer-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Code, LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class AwsChromeLambdaLayerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsChromeLambdaLayerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new LayerVersion(this, "aws-chrome-lambda-layer", {
+      layerVersionName: "AwsChromeLambdaLayer",
+      compatibleRuntimes: [Runtime.NODEJS_18_X],
+      code: Code.fromAsset("./dist/layer"),
+    });
   }
 }
---
+++ src/aws-chrome-lambda-layer.ts
@@ -0,0 +1,35 @@
+import chromium from "chrome-aws-lambda";
+
+export async function getPage(url: string) {
+  let browser: Awaited<
+    ReturnType<(typeof chromium)["puppeteer"]["launch"]>
+  > | null;
+  let page: Awaited<
+    ReturnType<
+      Awaited<ReturnType<(typeof chromium)["puppeteer"]["launch"]>>["newPage"]
+    >
+  >;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+    let page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle0" });
+
+    return page;
+  } catch (error) {
+    console.log("Warning:" + error);
+    console.log("Resetting browser context");
+    try {
+      page!.browser().close();
+    } catch {}
+  } finally {
+    browser = null;
+    return;
+  }
+}
